11839dbca87c87dcad14d92304a0a505c1f8fd43
NZpatelK <patelk.dev@hotmail.com>
2024-08-19 17:34:59 +1200

    Update ImagesZoomIn.tsx

--- src/components/images-animation/images-zoom-in/ImagesZoomIn.tsx
+++ src/components/images-animation/images-zoom-in/ImagesZoomIn.tsx
@@ -10,117 +10,88 @@ import img5 from '../../../assets/nature/5.jpeg';
 import img6 from '../../../assets/nature/6.jpeg';
 
 const CardZoomIn = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
+    const slides = gsap.utils.toArray<HTMLElement>(".slide");
+    const activeSlideImages = gsap.utils.toArray<HTMLElement>(".active-slide img");
 
-        const slides = gsap.utils.toArray<HTMLElement>(".slide");
-        const activeSlide = gsap.utils.toArray<HTMLElement>(".active-slide img");
+    const getInitialZ = (slide: HTMLElement) => {
+      const style = window.getComputedStyle(slide);
+      const matrix = style.transform.match(/^matrix3d\((.+)\)/);
 
-        const getInitialTranslateZ = (slide: any) => {
-            const style = window.getComputedStyle(slide);
-            const matrix = style.transform.match(/^matrix3d\((.+)\)/);
+      return matrix ? parseFloat(matrix[1].split(", ")[14]) : 0;
+    };
 
-            if (matrix) {
-                const values = matrix[1].split(', ');
-                return parseFloat(values[14]) || 0;
-            }
+    const mapRange = (
+      value: number,
+      inMin: number,
+      inMax: number,
+      outMin: number,
+      outMax: number
+    ) => ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 
-            return 0;
-        }
+    slides.forEach((slide, index) => {
+      const initialZ = getInitialZ(slide);
 
-        const mapRange = (value: any, inMin: any, inMax: any, outMin: any, outMax: any) => {
-            return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-        }
+      ScrollTrigger.create({
+        trigger: ".container",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true,
+        onUpdate: (self) => {
+          const progress = self.progress;
+          const zIncrement = progress * 12500;
+          const currentZ = initialZ + zIncrement;
 
-        slides.forEach((slide, index) => {
-            const initialZ = getInitialTranslateZ(slide);
+          let opacity;
 
-            ScrollTrigger.create({
-                trigger: ".container",
-                start: "top top",
-                end: "bottom bottom",
-                scrub: true,
-                onUpdate: (self) => {
-                    const progress = self.progress;
-                    const zIncrement = progress * 12500;
-                    const currentZ = initialZ + zIncrement;
+          if (currentZ > -2500) {
+            opacity = mapRange(currentZ, -2500, 0, 0.5, 1);
+          } else {
+            opacity = mapRange(currentZ, -5000, -2500, 0, 0.5);
+          }
 
-                    let opacity;
+          slide.style.opacity = opacity.toString();
+          slide.style.transform = `translate(-50%, -50%) translateZ(${currentZ}px)`;
 
-                    if (currentZ > -2500) {
-                        opacity = mapRange(currentZ, -2500, 0, 0.5, 1);
-                    } else {
-                        opacity = mapRange(currentZ, -5000, -2500, 0, 0.5);
-                    }
-
-                    slide.style.opacity = opacity
-                    slide.style.transform = `translate(-50%, -50%) translateZ(${currentZ}px)`;
-
-                    if (currentZ < 100) {
-                        gsap.to(activeSlide[index], 1.5, {
-                            opacity: 1,
-                            ease: "power3.out"
-                        });
-                    } else {
-                        gsap.to(activeSlide[index], 1.5, {
-                            opacity: 0,
-                            ease: "power3.out"
-                        });
-                    }
-                }
+          if (currentZ < 100) {
+            gsap.to(activeSlideImages[index], 1.5, {
+              opacity: 1,
+              ease: "power3.out",
             });
-        });
-    }, []);
+          } else {
+            gsap.to(activeSlideImages[index], 1.5, {
+              opacity: 0,
+              ease: "power3.out",
+            });
+          }
+        },
+      });
+    });
+  }, []);
 
-
-    return (
-        <div className='card-zoom-in'>
-            <div className="container">
-                <div className="active-slide">
-                    <img src={img1} alt="" />
-                    <img src={img2} alt="" />
-                    <img src={img3} alt="" />
-                    <img src={img4} alt="" />
-                    <img src={img5} alt="" />
-                    <img src={img6} alt="" />
-                </div>
-
-                <div className="slider">
-                    <div className="slide" id="slide-1">
-                        <div className="slide-img">
-                            <img src={img1} alt="" />
-                        </div>
-                    </div>
-                    <div className="slide" id="slide-2">
-                        <div className="slide-img">
-                            <img src={img2} alt="" />
-                        </div>
-                    </div>
-                    <div className="slide" id="slide-3">
-                        <div className="slide-img">
-                            <img src={img3} alt="" />
-                        </div>
-                    </div>
-                    <div className="slide" id="slide-4">
-                        <div className="slide-img">
-                            <img src={img4} alt="" />
-                        </div>
-                    </div>
-                    <div className="slide" id="slide-5">
-                        <div className="slide-img">
-                            <img src={img5} alt="" />
-                        </div>
-                    </div>
-                    <div className="slide" id="slide-6">
-                        <div className="slide-img">
-                            <img src={img6} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="card-zoom-in">
+      <div className="container">
+        <div className="active-slide">
+          {[img1, img2, img3, img4, img5, img6].map((image, index) => (
+            <img src={image} alt="" key={index} />
+          ))}
         </div>
-    );
+
+        <div className="slider">
+          {[img1, img2, img3, img4, img5, img6].map((image, index) => (
+            <div className="slide" id={`slide-${index + 1}`} key={index}>
+              <div className="slide-img">
+                <img src={image} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardZoomIn;
