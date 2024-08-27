b1f8ee4b3e2a8bc3c4608a62c69550ce8ff858ec
NZpatelK <patelk.dev@hotmail.com>
2024-08-27 12:37:43 +1200

    update teh style and image at sub card pop up

--- src/components/cards/subCard-popup/SubCardPopUp.tsx
+++ src/components/cards/subCard-popup/SubCardPopUp.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './SubCardPopUp.css';
+import img1 from '../../../assets/nature/1.jpeg';
+import img2 from '../../../assets/nature/2.jpeg';
+import img3 from '../../../assets/nature/3.jpeg';
 
 export default function SubCardPopUp() {
     return (
@@ -7,29 +10,32 @@ export default function SubCardPopUp() {
             <div className="subcard_card_container">
                 <article className="card_article">
                     <div className="card_img">
+                        <img src={img1} alt="" />
                     </div>
                     <div className="card_content">
-                        <span className="card_description"> Description of the card here </span>
-                        <h2 className="subCard_title"> Card Title </h2>
-                        <a href="#" className='card_link'>Read More</a>
+                        {/* <span className="card_description"> Description of the card here </span> */}
+                        <h2 className="subCard_title"> Water Fall </h2>
+                        {/* <a href="#" className='card_link'>Read More</a> */}
                     </div>
                 </article>
                 <article className="card_article">
                     <div className="card_img">
+                        <img src={img2} alt="" />
                     </div>
                     <div className="card_content">
-                        <span className="card_description"> Description of the card here </span>
-                        <h2 className="subCard_title"> Card Title </h2>
-                        <a href="#" className='card_link'>Read More</a>
+                        {/* <span className="card_description"> Description of the card here </span> */}
+                        <h2 className="subCard_title"> Nature Green </h2>
+                        {/* <a href="#" className='card_link'>Read More</a> */}
                     </div>
                 </article>
                 <article className="card_article">
                     <div className="card_img">
+                        <img src={img3} alt="" />
                     </div>
                     <div className="card_content">
-                        <span className="card_description"> Description of the card here </span>
-                        <h2 className="subCard_title"> Card Title </h2>
-                        <a href="#" className='card_link'>Read More</a>
+                        {/* <span className="card_description"> Description of the card here </span> */}
+                        <h2 className="subCard_title"> Sunrise Sunset </h2>
+                        {/* <a href="#" className='card_link'>Read More</a> */}
                     </div>
                 </article>
             </div>
